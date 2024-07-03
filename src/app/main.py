cec1a9e511d72cd5f8f281506bb6f9c39e3f3501
Anna <pompeeva.anna@gmail.com>
2024-07-03 13:23:10 +0000

    Реализован класс FaceVerificationService

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,14 @@
-import logging
+from deepface import DeepFace  # type: ignore
 
 
-def get_greetings() -> str:
-    """Возвращает строку приветствия."""
-    return 'Hello World!'
+class FaceVerificationService:
+    """Cервис преобразования лица на фото в вектор."""
+
+    @staticmethod
+    def get_embedding(img_path: str):
+        """Генерация вектора на основе фотографии."""
+        return DeepFace.represent(img_path=img_path, model_name='Facenet')
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    logging.info(get_greetings())
+    FaceVerificationService.get_embedding('src/app/img.jpg')
