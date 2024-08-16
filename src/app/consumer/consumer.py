ef10979abd763c8a23404f14f9a0caa379b8b7a1
Anna <pompeeva.anna@gmail.com>
2024-08-16 10:28:23 +0000

    Fix flake8 errors

--- src/app/consumer/consumer.py
+++ src/app/consumer/consumer.py
@@ -1,50 +1,56 @@
-from aiokafka import AIOKafkaConsumer
 import asyncio
-from app.config import KAFKA_HOST, KAFKA_PORT, TOPIC
-import brotli
 import logging
+
+import brotli
+from aiokafka import AIOKafkaConsumer
+
+from app.config import KAFKA_HOST, KAFKA_PORT, TOPIC
 from app.core.service import FaceVerificationService
 
 event_loop = asyncio.get_event_loop()  # ???
-file_encoding = "utf-8"
+file_encoding = 'utf-8'
 file_compression_quality = 1
 
 
-class Consumer(object):
+class Consumer:
+    """Класс для чтения сообщений из kafka."""
+
     def __init__(self):
-        self.__consumer = AIOKafkaConsumer(TOPIC,
-                                           bootstrap_servers=f"{
-                                               KAFKA_HOST}:{KAFKA_PORT}",
-                                           loop=event_loop,
-                                           )
+        """Инициализация консьюмера."""
+        self.__consumer = AIOKafkaConsumer(
+            TOPIC,
+            bootstrap_servers=f'{KAFKA_HOST}:{KAFKA_PORT}',
+            loop=event_loop,
+        )
 
     async def start(self) -> None:
+        """Запуск консьюмера."""
         await self.__consumer.start()
 
     async def stop(self) -> None:
+        """Остановка консьюмера."""
         await self.__consumer.stop()
 
     async def decompress(self, file_bytes: bytes) -> str:
-        """Decompress message from Kafka."""
-
+        """Распаковка сообщение от Кафки."""
         return str(
             brotli.decompress(file_bytes),
             file_encoding,
         )
 
     async def consume(self):
-        """Consume and print messages from Kafka."""
+        """Получение и печать сообщения от Kafka."""
         async for msg in self.__consumer:
-            value = await self.decompress(msg.value)
-            user_id, img_path = value.split(':')
+            msg_value = await self.decompress(msg.value)
+            user_id, img_path = msg_value.split(':')
             embedding = FaceVerificationService.verify_user(img_path, int(user_id))
             if embedding is not None:
-                logging.info(f'face_vector: {str(embedding[0]['embedding'])}')
+                logging.info(f"face_vector: {str(embedding[0]['embedding'])}")
             else:
                 logging.info(f'Unable get face vector for id: {user_id}')
 
     async def health_check(self) -> bool:
-        """Checks if Kafka is available by fetching all metadata from the Kafka client."""
+        """Проверяет, доступен ли Kafka, получая все метаданные от клиента Kafka."""
         try:
             await self.__consumer.client.fetch_all_metadata()
         except Exception as exc:
@@ -55,6 +61,7 @@ class Consumer(object):
 
 
 def get_consumer() -> Consumer:
+    """Получение экземпляра класса Consumer."""
     return Consumer()
 
 
